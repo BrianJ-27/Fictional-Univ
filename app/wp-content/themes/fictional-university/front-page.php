c9b1e4b9eec2ae60d2eede382b8cc62c19ff5fe9
Brian Johnson <johnson8727@hotmail.com>
2020-03-25 19:37:01 -0400

    responsive mobile animal hero design

--- app/wp-content/themes/fictional-university/front-page.php
+++ app/wp-content/themes/fictional-university/front-page.php
@@ -6,7 +6,7 @@
 get_header(); ?>
 
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/nature-hero.jpg') ?> );"></div>
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/nature-hero.jpg') ?> ); background-position: top"></div>
     <div class="page-banner__content container t-center c-white">
       <h1 class="headline headline--large">Welcome</h1>
       <h2 class="headline headline--medium">Where IQ&rsquo;s and Instincts Come Together</h2>
